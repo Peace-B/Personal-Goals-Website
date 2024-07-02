0c193668c365662953fe4fdcfd0a3312e86bfcf1
peace <peacebizzy32@gmail.com>
2024-07-02 19:28:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function updateTime() {
+    const now = new Date();
+    const currentTimeUTC = now.toISOString().split('T')[1].slice(0, 8);
+    document.querySelector('[data-testid="currentTimeUTC"]').innerText = currentTimeUTC;
+}
+
+function updateDay() {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = days[new Date().getUTCDay()];
+    document.querySelector('[data-testid="currentDay"]').innerText = currentDay;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateTime();
+    updateDay();
+    setInterval(updateTime, 1000);  
+});
